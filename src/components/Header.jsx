705115e869b422d9e2322a7eef488210b0f40088
Inioluwa atanda <129838510+Eunini@users.noreply.github.com>
2024-08-23 20:22:46 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'; 
-import ThemeToggle from './ThemeToggle'; 
+import ThemeToggle from './Themetoggle'; 
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
