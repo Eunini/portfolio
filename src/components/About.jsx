7f2b270f3af0cc67a324510597263769c6f4afd7
Inioluwa atanda <129838510+Eunini@users.noreply.github.com>
2024-09-11 22:09:49 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
             <h2 data-aos="fade-up" data-aos-duration="500">{typedHeading}</h2>
             <p className={slideIn ? 'slide-in' : ''} data-aos="zoom-in">{aboutText}</p>
             <div className="download-container" data-aos="zoom-in">
-               < a href= "../../public/images/Resume.pdf"
+               <a href= "/images/Resume.pdf"
   className="download-button" download>
                     Download my resume <FaDownload size={16} />
                 </a>
